f9a2ab81af49b3d44c9320f674f5d18e2fdff306
Steph <stephenbaguisi@gmail.com>
2026-06-14 15:25:25 +0800

    branch

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ int main(){
 
     std::cout << "hi" << std::endl;
     std::cout << "Hello" << std::endl;
-    std:: cout << "INAKA" << std :: endl;
+    std:: cout << "INAKA" << std::endl;
+    std:: cout << "loko ka ahh" << std::endl;
+
     return 0;
 }
